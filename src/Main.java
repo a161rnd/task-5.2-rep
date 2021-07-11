419af0f3b9c299e2ee1cf21eb9258ed6b1fe4ca7
Администратор <a161rnd@gmail.com>
2021-07-11 19:45:22 +0300

    one fix

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         String onestring = "not use";
-        String version = "master banch";
+        String version = "master branch";
     }
 }
