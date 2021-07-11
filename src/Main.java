76a228cced13d2e23acef9ed06103a0a1ac2b528
Администратор <a161rnd@gmail.com>
2021-07-11 19:49:29 +0300

    branch two first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String onestring = "not use";
-        String version = "master branch";
+        String onestring = "use";
+        String version = "branch two";
     }
 }
